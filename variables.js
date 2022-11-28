5dd1ebef5c6709c82a017c9f0bbe3708700e1b20
Mohamed Matano <matanomohamed3@gmail.com>
2022-11-28 22:16:53 +0300

    first use of javascript

---
+++ variables.js
@@ -0,0 +1,16 @@
+var first_name;
+first_name = 'Mohamed';
+var last_name;
+last_name = 'Matano';
+var a = 20;
+var b = 10;
+var c = a + b;
+var d = a - b;
+
+alert ('First use of javascript');
+
+console.log ('My names are:'+first_name +last_name);
+console.log ('Value of addition is:' +c);
+console.log ('Value of subtraction is:' +d);
+console.log ('Value of multiplication is:' +a * +b);
+console.log ('Value of division is:' +a / +b);
